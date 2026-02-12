9844ce508236dba49f65ee653c8418cf83d3e1c4
rramosme <rramosme@outlook.com>
2026-02-12 06:53:14 -0500

    Primer commit - Raul Fernando Ramos Mejia

---
+++ src/main/java/com/T2_RAUL_FERNANDO_RAMOS_MEJIA/App.java
@@ -0,0 +1,13 @@
+package com.T2_RAUL_FERNANDO_RAMOS_MEJIA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
